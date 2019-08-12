fe08b1116d94f560f2beaefd305984aeb3b2b987
dana <you@example.com>
2019-08-12 18:51:03 +0300

    change name to pl

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,26 +1,25 @@
 import React from 'react';
 
-
-
 const Skills = (props) => {
-   const mySkills = (
-     <div>
-       {props.Skills.map((edu) =>
-         <div className='item' key={edu.degree}>
-           <h3>{edu.degree} @ {edu.institution} <span>{edu.startDate} - {edu.endDate}</span></h3>
-           <p>{edu.description}</p>
-         </div>
-       )}
-     </div>
-   );
-   return (
-     <div className='title'>
-       <i class = "fa fa-file-code-o"></i>
-       <h2>SKILLS</h2>
-       {mySkills}
-     </div>
-   )
- };
-
+  const mySkills = (
+    <div>
+      {props.skills.map((skll) =>
+        <div className='item w33' key={skll.pl}>
+          <h3>{skll.pl}</h3>
+          <div className='skills'>
+            <span style={{width: skll.percentage}}></span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+  return (
+    <div className='title'>
+      <i className='fa fa-code'></i>
+      <h2>SKILLS</h2>
+      {mySkills}
+    </div>
+  )
+};
 
 export default Skills;
