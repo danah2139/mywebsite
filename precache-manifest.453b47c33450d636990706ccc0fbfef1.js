2aae643f00320f6e8d7a6b65295651f54abbd7e0
dana <you@example.com>
2019-08-20 17:36:01 +0300

    Updates

---
+++ precache-manifest.453b47c33450d636990706ccc0fbfef1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e7aea970dca7865ab94dfdbd0d29a3",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "c84ea785eb6c67060ebd",
+    "url": "/mywebsite/static/js/2.c35e57ec.chunk.js"
+  },
+  {
+    "revision": "6894271f18e4cf51f123",
+    "url": "/mywebsite/static/js/main.e9b6cb12.chunk.js"
+  },
+  {
+    "revision": "1029a0e86ffa031e1963",
+    "url": "/mywebsite/static/js/runtime~main.8e5ad122.js"
+  },
+  {
+    "revision": "e564b00b8540c21e5a593a282d7e90e9",
+    "url": "/mywebsite/static/media/Dana-Cohen.e564b00b.jpg"
+  }
+]);
