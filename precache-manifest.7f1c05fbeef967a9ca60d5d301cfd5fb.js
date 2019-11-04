cca815880e36e0d539678de4fb35d30e745c1e22
danah2139 <danah2139@gmail.com>
2019-11-04 18:57:52 +0200

    Updates

---
+++ precache-manifest.7f1c05fbeef967a9ca60d5d301cfd5fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f83b78d69a256982ffc",
+    "url": "/static/js/main.db7c6ff7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "66bbc66fb81735725762",
+    "url": "/static/js/2.4798774d.chunk.js"
+  },
+  {
+    "revision": "e564b00b8540c21e5a593a282d7e90e9",
+    "url": "/static/media/Dana-Cohen.e564b00b.jpg"
+  },
+  {
+    "revision": "81352c4677066f8a56a3e53ad204e05e",
+    "url": "/index.html"
+  }
+];
